f6d6059c3e7084448c1bb329e366e6186a3d25d4
michimimimi <car.carrascoe@duocuc.cl>
2024-05-07 20:13:14 -0400

    Add files via upload

---
+++ MENU_mikaela.py
@@ -0,0 +1,27 @@
+#primero ordenar el menu
+print("1.Pikachu roll $4500")
+print("2.Otaku roll $5000")
+print("3.Pulpo venenoso roll $5200")
+print("4.Anguila electrica roll $4800")
+
+#luego solicitar la opcion
+opcion=input("ingrese su opcion: ")
+print("usted ha elegido la opcion: " + opcion)
+
+#luego el codigo de descuento
+cod=input("ingrese el codigo de descuento: ")
+if cod:("soyotaku")
+print("usted tiene un 10% de descuento")
+else:
+print("codigo invalido")
+cod=input("vuelva a ingresar el codigo: ")
+
+
+
+
+
+
+
+
+
+
